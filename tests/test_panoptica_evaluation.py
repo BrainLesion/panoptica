d54c9e9eacf19f3591c979c99d8614f34e503282
neuronflow <florian.kofler@tum.de>
2023-10-20 15:02:37 +0200

    new: tests
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- tests/test_panoptica_evaluation.py
+++ tests/test_panoptica_evaluation.py
@@ -1,12 +1,23 @@
 import numpy as np
 import pytest
+from panoptica.panoptica_evaluation import panoptica_evaluation
 
-from panoptica.panoptic_quality import panoptic_quality
 
-
+# Test fixtures to generate 2D and 3D test data
 @pytest.fixture
 def generate_test_data_2d():
     def _generate_test_data(num_ref_instances, num_pred_instances, shape=(100, 100)):
+        """
+        Generate test data for 2D scenarios.
+
+        Args:
+            num_ref_instances (int): Number of reference instances.
+            num_pred_instances (int): Number of predicted instances.
+            shape (tuple): Shape of the 2D mask.
+
+        Returns:
+            tuple: A tuple containing reference masks, predicted masks, reference instances, and predicted instances.
+        """
         ref_masks = np.zeros(shape, dtype=np.uint8)
         pred_masks = np.zeros(shape, dtype=np.uint8)
 
@@ -34,6 +45,17 @@ def generate_test_data_3d():
     def _generate_test_data(
         num_ref_instances, num_pred_instances, shape=(100, 100, 100)
     ):
+        """
+        Generate test data for 3D scenarios.
+
+        Args:
+            num_ref_instances (int): Number of reference instances.
+            num_pred_instances (int): Number of predicted instances.
+            shape (tuple): Shape of the 3D mask.
+
+        Returns:
+            tuple: A tuple containing reference masks, predicted masks, reference instances, and predicted instances.
+        """
         ref_masks = np.zeros(shape, dtype=np.uint8)
         pred_masks = np.zeros(shape, dtype=np.uint8)
 
@@ -65,114 +87,130 @@ def generate_test_data_3d():
 
 
 # Test cases
-def test_compute_panoptic_quality_instances_2d_no_overlap(generate_test_data_2d):
-    # Test cases for 2D data with no overlap
+def test_compute_panoptica_quality_instances_2d_no_overlap(generate_test_data_2d):
+    """
+    Test cases for 2D data with no instance overlap.
+    """
     ref_masks, pred_masks, _, _ = generate_test_data_2d(0, 0, shape=(100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert pq == 1.0
-    assert sq == 1.0
-    assert rq == 1.0
-    assert tp == 0
-    assert fp == 0
-    assert fn == 0
+    assert result.pq == 0.0
+    assert result.sq == 0.0
+    assert result.rq == 0.0
+    assert result.tp == 0
+    assert result.fp == 0
+    assert result.fn == 0
 
     ref_masks, pred_masks, _, _ = generate_test_data_2d(0, 5, shape=(100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert pq == 0.0
-    assert sq == 0.0
-    assert rq == 0.0
-    assert tp == 0
-    assert fp == 5
-    assert fn == 0
+    assert result.pq == 0.0
+    assert result.sq == 0.0
+    assert result.rq == 0.0
+    assert result.tp == 0
+    assert result.fp == 5
+    assert result.fn == 0
 
     ref_masks, pred_masks, _, _ = generate_test_data_2d(5, 0, shape=(100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert pq == 0.0
-    assert sq == 0.0
-    assert rq == 0.0
-    assert tp == 0
-    assert fp == 0
-    assert fn == 5
+    assert result.pq == 0.0
+    assert result.sq == 0.0
+    assert result.rq == 0.0
+    assert result.tp == 0
+    assert result.fp == 0
+    assert result.fn == 5
 
 
-def test_compute_panoptic_quality_instances_2d_overlap(generate_test_data_2d):
-    # Test case for 2D data with overlap (more meaningful test required)
+def test_compute_panoptica_quality_instances_2d_overlap(generate_test_data_2d):
+    """
+    Test cases for 2D data with instance overlap.
+    """
     ref_masks, pred_masks, _, _ = generate_test_data_2d(5, 5, shape=(100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert 0.0 <= pq <= 1.0
-    assert 0.0 <= sq <= 1.0
-    assert 0.0 <= rq <= 1.0
+    assert 0.0 <= result.pq <= 1.0
+    assert 0.0 <= result.sq <= 1.0
+    assert 0.0 <= result.rq <= 1.0
 
 
-def test_compute_panoptic_quality_instances_3d_no_overlap(generate_test_data_3d):
-    # Test cases for 3D data with no overlap
+def test_compute_panoptica_quality_instances_3d_no_overlap(generate_test_data_3d):
+    """
+    Test cases for 3D data with no instance overlap.
+    """
     ref_masks, pred_masks, _, _ = generate_test_data_3d(0, 0, shape=(100, 100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert pq == 1.0
-    assert sq == 1.0
-    assert rq == 1.0
-    assert tp == 0
-    assert fp == 0
-    assert fn == 0
+    assert result.pq == 0.0
+    assert result.sq == 0.0
+    assert result.rq == 0.0
+    assert result.tp == 0
+    assert result.fp == 0
+    assert result.fn == 0
 
     ref_masks, pred_masks, _, _ = generate_test_data_3d(0, 5, shape=(100, 100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert pq == 0.0
-    assert sq == 0.0
-    assert rq == 0.0
-    assert tp == 0
-    assert fp == 5
-    assert fn == 0
+    assert result.pq == 0.0
+    assert result.sq == 0.0
+    assert result.rq == 0.0
+    assert result.tp == 0
+    assert result.fp == 5
+    assert result.fn == 0
 
     ref_masks, pred_masks, _, _ = generate_test_data_3d(5, 0, shape=(100, 100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert pq == 0.0
-    assert sq == 0.0
-    assert rq == 0.0
-    assert tp == 0
-    assert fp == 0
-    assert fn == 5
+    assert result.pq == 0.0
+    assert result.sq == 0.0
+    assert result.rq == 0.0
+    assert result.tp == 0
+    assert result.fp == 0
+    assert result.fn == 5
 
 
-def test_compute_panoptic_quality_instances_3d_overlap(generate_test_data_3d):
-    # Test case for 3D data with overlap (more meaningful test required)
+def test_compute_panoptica_quality_instances_3d_overlap(generate_test_data_3d):
+    """
+    Test cases for 3D data with instance overlap.
+    """
     ref_masks, pred_masks, _, _ = generate_test_data_3d(5, 5, shape=(100, 100, 100))
-    pq, sq, rq, tp, fp, fn = panoptic_quality(
+    result = panoptica_evaluation(
         ref_mask=ref_masks,
         pred_mask=pred_masks,
+        iou_threshold=0.5,
         modus="cc",
     )
-    assert 0.0 <= pq <= 1.0
-    assert 0.0 <= sq <= 1.0
-    assert 0.0 <= rq <= 1.0
+    assert 0.0 <= result.pq <= 1.0
+    assert 0.0 <= result.sq <= 1.0
+    assert 0.0 <= result.rq <= 1.0
 
 
 if __name__ == "__main__":
