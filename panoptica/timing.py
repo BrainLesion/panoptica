470ba61bcc07e36ea00f35a08ba5260e82051386
neuronflow <florian.kofler@tum.de>
2023-10-20 14:43:46 +0200

    timings
    Signed-off-by: neuronflow <florian.kofler@tum.de>

---
+++ panoptica/timing.py
@@ -0,0 +1,13 @@
+import time
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__} took {elapsed_time} seconds to execute.")
+        return result
+
+    return wrapper
