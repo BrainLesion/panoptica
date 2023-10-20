9625364deb74adcd7f7b672c04e53e6dadd82b39
neuronflow <florian.kofler@tum.de>
2023-10-20 15:58:39 +0200

    version bump and more results
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- tests/test_cfos_3d.py
+++ tests/test_cfos_3d.py
@@ -1,18 +1,12 @@
 from auxiliary.nifti.io import read_nifti
-from auxiliary.turbopath import turbopath
-
 from panoptica.panoptica_evaluation import panoptica_evaluation
 
 
 pred_masks = read_nifti(
-    input_nifti_path=turbopath(
-        "/home/florian/flow/cfos_analysis/data/ablation/2021-11-25_23-50-56_2021-10-25_19-38-31_tr_dice_bce_11/patchvolume_695_2.nii.gz"
-    )
+    input_nifti_path="/home/florian/flow/cfos_analysis/data/ablation/2021-11-25_23-50-56_2021-10-25_19-38-31_tr_dice_bce_11/patchvolume_695_2.nii.gz"
 )
 ref_masks = read_nifti(
-    input_nifti_path=turbopath(
-        "/home/florian/flow/cfos_analysis/data/reference/patchvolume_695_2/patchvolume_695_2_binary.nii.gz",
-    )
+    input_nifti_path="/home/florian/flow/cfos_analysis/data/reference/patchvolume_695_2/patchvolume_695_2_binary.nii.gz",
 )
 
 # Call panoptica_quality to obtain the result
@@ -23,7 +17,6 @@ result = panoptica_evaluation(
     modus="cc",
 )
 
-
 # Print the metrics
 print("Panoptic Quality (PQ):", result.pq)
 print("Segmentation Quality (SQ):", result.sq)
@@ -32,3 +25,5 @@ print("True Positives (tp):", result.tp)
 print("False Positives (fp):", result.fp)
 print("False Negatives (fn):", result.fn)
 print("instance_dice", result.i_dice)
+print("number of instances in prediction:", result.num_pred_instances)
+print("number of instances in reference:", result.num_ref_instances)
