dc9971fc6913a66d53b6b7f55c3d9eb24aa67abe
neuronflow <florian.kofler@tum.de>
2023-10-25 22:21:58 +0200

    instance metrics
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- panoptica/instance.py
+++ panoptica/instance.py
@@ -5,9 +5,6 @@ import warnings
 
 import numpy as np
 
-
-from scipy.optimize import linear_sum_assignment
-
 from .timing import measure_time
 
 
@@ -39,9 +36,10 @@ class InstanceSegmentationEvaluator(Evaluator):
             num_pred_instances=num_pred_instances,
         )
 
-        # Initialize variables for True Positives (tp) and False Positives (fp)
-        tp, fp, dice_list, iou_list = 0, 0, [], []
+        # Initialize variables for True Positives (tp)
+        tp, dice_list, iou_list = 0, [], []
 
+        # TODO parallelize this loop
         # loop through all reference labels and compute iou
         for ref_idx in ref_nonzero_unique_labels:
             iou = self._compute_iou(
@@ -52,36 +50,17 @@ class InstanceSegmentationEvaluator(Evaluator):
                 iou_list.append(iou)
                 tp += 1
 
-                
-
-            # compute other instance metrics
-
-        # Loop through matched instances to compute PQ components
-        for ref_idx, pred_idx in zip(ref_indices, pred_indices):
-            iou = iou_matrix[ref_idx][pred_idx]
-            if iou >= iou_threshold:
-                # Match found, increment true positive count and collect IoU and Dice values
-                tp += 1
-                iou_list.append(iou)
-
-                # Compute Dice for matched instances
-                dice = self._compute_instance_volumetric_dice(
-                    ref_labels=ref_labels,
-                    pred_labels=pred_labels,
-                    ref_instance_idx=ref_idx + 1,
-                    pred_instance_idx=pred_idx + 1,
+                dice = self._compute_dice_coefficient(
+                    reference=ref_labels == ref_idx,
+                    prediction=pred_labels == ref_idx,
                 )
                 dice_list.append(dice)
-            else:
-                # No match found, increment false positive count
-                fp += 1
 
         # Create and return the PanopticaResult object with computed metrics
         return PanopticaResult(
             num_ref_instances=num_ref_instances,
             num_pred_instances=num_pred_instances,
             tp=tp,
-            fp=fp,
             dice_list=dice_list,
             iou_list=iou_list,
         )
