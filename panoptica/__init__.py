92fb02e1f02315dd29b1f88f469bb0719e168b58
neuronflow <florian.kofler@tum.de>
2023-11-04 04:25:15 +0100

    back to instance
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- panoptica/__init__.py
+++ panoptica/__init__.py
@@ -1,7 +1,3 @@
-from panoptica.instance_segmentation.instance_evaluator import (
-    InstanceSegmentationEvaluator,
-)
-from panoptica.semantic_segmentation.connected_component_backends import CCABackend
-from panoptica.semantic_segmentation.semantic_evaluator import (
-    SemanticSegmentationEvaluator,
-)
+from panoptica.instance.instance_evaluator import InstanceSegmentationEvaluator
+from panoptica.semantic.connected_component_backends import CCABackend
+from panoptica.semantic.semantic_evaluator import SemanticSegmentationEvaluator
