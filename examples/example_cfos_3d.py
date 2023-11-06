fe3df9f2bc83dcaae5cc21baac6cfcead4b1debd
neuronflow <florian.kofler@tum.de>
2023-11-06 10:12:24 +0100

    new:restructure names
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- examples/example_cfos_3d.py
+++ examples/example_cfos_3d.py
@@ -1,7 +1,6 @@
 from auxiliary.nifti.io import read_nifti
 
-from panoptica.panoptica_evaluation import panoptica_evaluation
-from panoptica.semantic import SemanticSegmentationEvaluator
+from panoptica import CCABackend, SemanticSegmentationEvaluator
 
 pred_masks = read_nifti(
     input_nifti_path="/home/florian/flow/cfos_analysis/data/ablation/2021-11-25_23-50-56_2021-10-25_19-38-31_tr_dice_bce_11/patchvolume_695_2.nii.gz"
@@ -10,27 +9,7 @@ ref_masks = read_nifti(
     input_nifti_path="/home/florian/flow/cfos_analysis/data/reference/patchvolume_695_2/patchvolume_695_2_binary.nii.gz",
 )
 
-# Call panoptica_quality to obtain the result
-result = panoptica_evaluation(
-    ref_mask=ref_masks,
-    pred_mask=pred_masks,
-    iou_threshold=0.5,
-    modus="cc",
-)
-
-# Print the metrics
-print("Panoptic Quality (PQ):", result.pq)
-print("Segmentation Quality (SQ):", result.sq)
-print("Recognition Quality (RQ):", result.rq)
-print("True Positives (tp):", result.tp)
-print("False Positives (fp):", result.fp)
-print("False Negatives (fn):", result.fn)
-print("instance_dice", result.instance_dice)
-print("number of instances in prediction:", result.num_pred_instances)
-print("number of instances in reference:", result.num_ref_instances)
-
-
-eva = SemanticSegmentationEvaluator(cca_backend="cc3d")
+eva = SemanticSegmentationEvaluator(cca_backend=CCABackend.cc3d)
 res = eva.evaluate(
     reference_mask=ref_masks,
     prediction_mask=pred_masks,
