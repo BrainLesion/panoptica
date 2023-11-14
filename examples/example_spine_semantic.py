2d6941bb862e2ed7a8a1bb67146254f23a4454a4
neuronflow <7048826+neuronflow@users.noreply.github.com>
2023-11-14 10:08:43 +0100

    fixed: examples
    Signed-off-by: neuronflow <7048826+neuronflow@users.noreply.github.com>

--- examples/example_spine_semantic.py
+++ examples/example_spine_semantic.py
@@ -10,23 +10,27 @@ from panoptica import (
     SemanticPair,
 )
 
-directory = turbopath(__file__).parent
+if __name__ == "__main__":
+    directory = turbopath(__file__).parent
 
-ref_masks = read_nifti(directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk.nii.gz")
-pred_masks = read_nifti(directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk_new.nii.gz")
+    ref_masks = read_nifti(
+        directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk.nii.gz"
+    )
+    pred_masks = read_nifti(
+        directory
+        + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk_new.nii.gz"
+    )
 
+    sample = SemanticPair(pred_masks, ref_masks)
 
-sample = SemanticPair(pred_masks, ref_masks)
+    evaluator = Panoptic_Evaluator(
+        expected_input=SemanticPair,
+        instance_approximator=ConnectedComponentsInstanceApproximator(),
+        instance_matcher=NaiveOneToOneMatching(),
+        iou_threshold=0.5,
+    )
+    with cProfile.Profile() as pr:
+        result, debug_data = evaluator.evaluate(sample)
+        print(result)
 
-
-evaluator = Panoptic_Evaluator(
-    expected_input=SemanticPair,
-    instance_approximator=ConnectedComponentsInstanceApproximator(),
-    instance_matcher=NaiveOneToOneMatching(),
-    iou_threshold=0.5,
-)
-with cProfile.Profile() as pr:
-    result, debug_data = evaluator.evaluate(sample)
-    print(result)
-
-pr.dump_stats(directory + "/semantic_example.log")
+    pr.dump_stats(directory + "/semantic_example.log")
