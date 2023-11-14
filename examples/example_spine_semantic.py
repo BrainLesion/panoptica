4857ea77062f142c689defd1970268be77c18cc3
neuronflow <7048826+neuronflow@users.noreply.github.com>
2023-11-14 12:51:53 +0100

    fixed example
    Signed-off-by: neuronflow <7048826+neuronflow@users.noreply.github.com>

--- examples/example_spine_semantic.py
+++ examples/example_spine_semantic.py
@@ -10,13 +10,16 @@ from panoptica import (
     SemanticPair,
 )
 
-if __name__ == "__main__":
-    directory = turbopath(__file__).parent
+directory = turbopath(__file__).parent
 
-    ref_masks = read_nifti(directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk.nii.gz")
-    pred_masks = read_nifti(directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk_new.nii.gz")
+ref_masks = read_nifti(
+    directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk.nii.gz"
+)
+pred_masks = read_nifti(
+    directory + "/spine_seg/semantic_example/sub-0007_mod-T2w_seg-spine_msk_new.nii.gz"
+)
 
-    sample = SemanticPair(pred_masks, ref_masks)
+sample = SemanticPair(pred_masks, ref_masks)
 
 evaluator = Panoptic_Evaluator(
     expected_input=SemanticPair,
@@ -25,7 +28,8 @@ evaluator = Panoptic_Evaluator(
     iou_threshold=0.5,
 )
 with cProfile.Profile() as pr:
-    result, debug_data = evaluator.evaluate(sample)
-    print(result)
+    if __name__ == "__main__":
+        result, debug_data = evaluator.evaluate(sample)
+        print(result)
 
-    pr.dump_stats(directory + "/semantic_example.log")
+        pr.dump_stats(directory + "/semantic_example.log")
