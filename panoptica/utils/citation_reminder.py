d34ed16229db19a0424ca161a7a42cbe8ec56df7
Marcel Rosier <marcel.rosier@gmx.de>
2023-12-07 14:32:58 +0100

    - added env var to disable reminder

--- panoptica/utils/citation_reminder.py
+++ panoptica/utils/citation_reminder.py
@@ -1,3 +1,4 @@
+import os
 from rich.console import Console
 
 CITATION_LINK = "https://github.com/BrainLesion/panoptica#citation"
@@ -5,12 +6,13 @@ CITATION_LINK = "https://github.com/BrainLesion/panoptica#citation"
 
 def citation_reminder(func):
     def wrapper(*args, **kwargs):
-        console = Console()
-        console.rule("[bold] Citation reminder [/bold]")
-        console.print(
-            f"If you use this software in your research, please [bold]cite[/bold] [italic]{CITATION_LINK}[italic]", justify="center")
-        console.print("Thank you!", justify="center")
-        console.rule()
-        console.line()
+        if os.environ.get("PANOPTICA_CITATION_REMINDER", "true").lower() == "true":
+            console = Console()
+            console.rule("[bold] Citation reminder [/bold]")
+            console.print(
+                f"If you use this software in your research, please [bold]cite[/bold] [italic]{CITATION_LINK}[italic]", justify="center")
+            console.print("Thank you!", justify="center")
+            console.rule()
+            console.line()
         return func(*args, **kwargs)
     return wrapper
