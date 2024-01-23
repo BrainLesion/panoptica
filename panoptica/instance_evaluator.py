merged matchingmetric and listmetric into one called Metric, fixed bug where metric computation error reason was not shown properly. started unittest for datatypes

panoptica/instance_evaluator.py
@@ -1,23 +1,16 @@
-import concurrent.futures
-import gc
 from multiprocessing import Pool
-
 import numpy as np
 
-from panoptica.metrics import (
-    _MatchingMetric,
-)
 from panoptica.panoptic_result import PanopticaResult
-from panoptica.timing import measure_time
 from panoptica.utils import EdgeCaseHandler
 from panoptica.utils.processing_pair import MatchedInstancePair
-from panoptica.metrics import MatchingMetrics, ListMetric
+from panoptica.metrics import Metric
 
 
 def evaluate_matched_instance(
     matched_instance_pair: MatchedInstancePair,
-    eval_metrics: list[_MatchingMetric] = [MatchingMetrics.DSC, MatchingMetrics.IOU, MatchingMetrics.ASSD],
-    decision_metric: _MatchingMetric | None = MatchingMetrics.IOU,
+    eval_metrics: list[Metric] = [Metric.DSC, Metric.IOU, Metric.ASSD],
+    decision_metric: Metric | None = Metric.IOU,
     decision_threshold: float | None = None,
     edge_case_handler: EdgeCaseHandler | None = None,
     **kwargs,
@@ -41,11 +34,10 @@ def evaluate_matched_instance(
         edge_case_handler = EdgeCaseHandler()
     if decision_metric is not None:
         assert decision_metric.name in [v.name for v in eval_metrics], "decision metric not contained in eval_metrics"
-        assert decision_metric.name in ListMetric.__members__, f"decision metric {decision_metric} not a member of ListMetric"
         assert decision_threshold is not None, "decision metric set but no threshold"
     # Initialize variables for True Positives (tp)
     tp = len(matched_instance_pair.matched_instances)
-    score_dict: dict[ListMetric, list[float]] = {ListMetric[m.name]: [] for m in eval_metrics}
+    score_dict: dict[Metric, list[float]] = {m: [] for m in eval_metrics}
 
     reference_arr, prediction_arr = (
         matched_instance_pair._reference_arr,
@@ -55,12 +47,12 @@ def evaluate_matched_instance(
 
     instance_pairs = [(reference_arr, prediction_arr, ref_idx, eval_metrics) for ref_idx in ref_matched_labels]
     with Pool() as pool:
-        metric_dicts = pool.starmap(_evaluate_instance, instance_pairs)
+        metric_dicts: list[dict[Metric, float]] = pool.starmap(_evaluate_instance, instance_pairs)
 
     for metric_dict in metric_dicts:
         if decision_metric is None or (
             decision_threshold is not None
-            and decision_metric.score_beats_threshold(metric_dict[ListMetric[decision_metric.name]], decision_threshold)
+            and decision_metric.score_beats_threshold(metric_dict[decision_metric], decision_threshold)
         ):
             for k, v in metric_dict.items():
                 score_dict[k].append(v)
@@ -81,8 +73,8 @@ def _evaluate_instance(
     reference_arr: np.ndarray,
     prediction_arr: np.ndarray,
     ref_idx: int,
-    eval_metrics: list[_MatchingMetric],
-) -> dict[ListMetric, float]:
+    eval_metrics: list[Metric],
+) -> dict[Metric, float]:
     """
     Evaluate a single instance.
 
@@ -97,17 +89,12 @@ def _evaluate_instance(
     """
     ref_arr = reference_arr == ref_idx
     pred_arr = prediction_arr == ref_idx
-    result: dict[ListMetric, float] = {}
+    result: dict[Metric, float] = {}
     if ref_arr.sum() == 0 or pred_arr.sum() == 0:
         return result
     else:
         for metric in eval_metrics:
-            try:
-                metric_name = ListMetric[metric.name]
-            except Exception as e:
-                print(f"metric with name {metric} not defined in ListMetric. Add it to it or remove it.")
-                raise e
-            value = metric._metric_function(ref_arr, pred_arr)
-            result[metric_name] = value
+            metric_value = metric(ref_arr, pred_arr)
+            result[metric] = metric_value
 
     return result

panoptica/metrics/metrics.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from enum import EnumMeta
-from typing import Callable
+from enum import EnumMeta, Enum
+from typing import Any, Callable
 
 import numpy as np
 
@@ -14,7 +14,9 @@ from panoptica.utils.constants import _Enum_Compare, auto
 
 
 @dataclass
-class _MatchingMetric:
+class _Metric:
+    """A Metric class containing a name, whether higher or lower values is better, and a function to calculate that metric between two instances in an array
+    """
     name: str
     decreasing: bool
     _metric_function: Callable
@@ -27,16 +29,16 @@ class _MatchingMetric:
         pred_instance_idx: int | list[int] | None = None,
         *args,
         **kwargs,
-    ):
+    ) -> int | float:
         if ref_instance_idx is not None and pred_instance_idx is not None:
             reference_arr = reference_arr.copy() == ref_instance_idx
             if isinstance(pred_instance_idx, int):
                 pred_instance_idx = [pred_instance_idx]
-            prediction_arr = np.isin(prediction_arr.copy(), pred_instance_idx)
+            prediction_arr = np.isin(prediction_arr.copy(), pred_instance_idx) #type:ignore
         return self._metric_function(reference_arr, prediction_arr, *args, **kwargs)
 
     def __eq__(self, __value: object) -> bool:
-        if isinstance(__value, _MatchingMetric):
+        if isinstance(__value, _Metric):
             return self.name == __value.name
         elif isinstance(__value, str):
             return self.name == __value
@@ -48,6 +50,9 @@ class _MatchingMetric:
 
     def __repr__(self) -> str:
         return str(self)
+    
+    def __hash__(self) -> int:
+        return abs(hash(self.name)) % (10**8)
 
     @property
     def increasing(self):
@@ -57,37 +62,96 @@ class _MatchingMetric:
         return (self.increasing and matching_score >= matching_threshold) or (self.decreasing and matching_score <= matching_threshold)
 
 
-# Important metrics that must be calculated in the evaluator, can be set for thresholding in matching and evaluation
-class MatchingMetrics:
-    DSC: _MatchingMetric = _MatchingMetric("DSC", False, _compute_dice_coefficient)
-    IOU: _MatchingMetric = _MatchingMetric("IOU", False, _compute_iou)
-    ASSD: _MatchingMetric = _MatchingMetric("ASSD", True, _average_symmetric_surface_distance)
-    clDSC: _MatchingMetric = _MatchingMetric("clDSC", False, _compute_centerline_dice_coefficient)
+class DirectValueMeta(EnumMeta):    
+    "Metaclass that allows for directly getting an enum attribute"
+    def __getattribute__(cls, name) -> _Metric:
+        value = super().__getattribute__(name)
+        if isinstance(value, cls):
+            value = value.value
+        return value
+    
 
-class ListMetricMode(_Enum_Compare):
+class Metric(_Enum_Compare):
+    """Enum containing important metrics that must be calculated in the evaluator, can be set for thresholding in matching and evaluation
+    Never call the .value member here, use the properties directly
+    
+    Returns:
+        _type_: _description_
+    """
+    DSC = _Metric("DSC", False, _compute_dice_coefficient)
+    IOU = _Metric("IOU", False, _compute_iou)
+    ASSD = _Metric("ASSD", True, _average_symmetric_surface_distance)
+    clDSC = _Metric("clDSC", False, _compute_centerline_dice_coefficient)
+
+    def __call__(
+        self,
+        reference_arr: np.ndarray,
+        prediction_arr: np.ndarray,
+        ref_instance_idx: int | None = None,
+        pred_instance_idx: int | list[int] | None = None,
+        *args,
+        **kwargs,
+    ) -> int | float:
+        """Calculates the underlaying metric
+
+        Args:
+            reference_arr (np.ndarray): Reference array
+            prediction_arr (np.ndarray): Prediction array
+            ref_instance_idx (int | None, optional): The index label to be evaluated for the reference. Defaults to None.
+            pred_instance_idx (int | list[int] | None, optional): The index label to be evaluated for the prediction. Defaults to None.
+
+        Returns:
+            int | float: The metric value
+        """
+        return self.value(reference_arr=reference_arr, prediction_arr=prediction_arr, ref_instance_idx=ref_instance_idx, pred_instance_idx=pred_instance_idx, *args, **kwargs,)
+
+    def score_beats_threshold(self, matching_score: float, matching_threshold: float) -> bool:
+        """Calculates whether a score beats a specified threshold
+
+        Args:
+            matching_score (float): Metric score
+            matching_threshold (float): Threshold to compare against
+
+        Returns:
+            bool: True if the matching_score beats the threshold, False otherwise.
+        """
+        return (self.increasing and matching_score >= matching_threshold) or (self.decreasing and matching_score <= matching_threshold)
+
+    @property
+    def name(self):
+        return self.value.name
+    
+    @property
+    def decreasing(self):
+        return self.value.decreasing
+
+    @property
+    def increasing(self):
+        return self.value.increasing
+
+    def __hash__(self) -> int:
+        return abs(hash(self.name)) % (10**8)
+
+
+class MetricMode(_Enum_Compare):
+    """Different modalities from Metrics
+
+    Args:
+        _Enum_Compare (_type_): _description_
+    """
     ALL = auto()
     AVG = auto()
     SUM = auto()
     STD = auto()
 
 
-class ListMetric(_Enum_Compare):
-    DSC = MatchingMetrics.DSC.name
-    IOU = MatchingMetrics.IOU.name
-    ASSD = MatchingMetrics.ASSD.name
-    clDSC = MatchingMetrics.clDSC.name
-
-    def __hash__(self) -> int:
-        return abs(hash(self.value)) % (10**8)
-
-
 if __name__ == "__main__":
-    print(MatchingMetrics.DSC)
+    print(Metric.DSC)
     # print(MatchingMetric.DSC.name)
 
-    print(MatchingMetrics.DSC == MatchingMetrics.DSC)
-    print(MatchingMetrics.DSC == "DSC")
-    print(MatchingMetrics.DSC.name == "DSC")
+    print(Metric.DSC == Metric.DSC)
+    print(Metric.DSC == "DSC")
+    print(Metric.DSC.name == "DSC")
     #
-    print(MatchingMetrics.DSC == MatchingMetrics.IOU)
-    print(MatchingMetrics.DSC == "IOU")
+    print(Metric.DSC == Metric.IOU)
+    print(Metric.DSC == "IOU")

panoptica/panoptic_result.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 from typing import Any, Callable
 import numpy as np
-from panoptica.metrics import ListMetricMode, ListMetric
+from panoptica.metrics import MetricMode, Metric
 from panoptica.metrics import _compute_dice_coefficient, _compute_centerline_dice_coefficient
 from panoptica.utils import EdgeCaseHandler
 from panoptica.utils.processing_pair import MatchedInstancePair
 
 
 class MetricCouldNotBeComputedException(Exception):
+    """Exception for when a Metric cannot be computed
+    """
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
 
@@ -36,13 +38,23 @@ class Evaluation_Metric:
         self.long_name = long_name
         self.was_calculated = was_calculated
         self.error = error
+        self.error_obj: MetricCouldNotBeComputedException | None = None
 
     def __call__(self, result_obj: PanopticaResult) -> Any:
         if self.error:
-            raise MetricCouldNotBeComputedException(f"Metric {self.id} requested, but could not be computed")
+            if self.error_obj is None:
+                raise MetricCouldNotBeComputedException(f"Metric {self.id} requested, but could not be computed")
+            else:
+                raise self.error_obj
         assert not self.was_calculated, f"Metric {self.id} was called to compute, but is set to have been already calculated"
         assert self.calc_func is not None, f"Metric {self.id} was called to compute, but has no calculation function set"
-        return self.calc_func(result_obj)
+        try:
+            value = self.calc_func(result_obj)
+        except MetricCouldNotBeComputedException as e:
+            value = e
+            self.error = True
+            self.error_obj = e
+        return value
 
     def __str__(self) -> str:
         if self.long_name is not None:
@@ -54,16 +66,16 @@ class Evaluation_Metric:
 class Evaluation_List_Metric:
     def __init__(
         self,
-        name_id: ListMetric,
+        name_id: Metric,
         empty_list_std: float | None,
         value_list: list[float] | None,  # None stands for not calculated
         is_edge_case: bool = False,
         edge_case_result: float | None = None,
     ):
-        """This represents the metrics resulting from a ListMetric (IoU, ASSD, Dice)
+        """This represents the metrics resulting from a Metric calculated between paired instances (IoU, ASSD, Dice, ...)
 
         Args:
-            name_id (ListMetric): code-name of this metric
+            name_id (Metric): code-name of this metric
             empty_list_std (float): Value for the standard deviation if the list of values is empty
             value_list (list[float] | None): List of values of that metric (only the TPs)
         """
@@ -78,10 +90,10 @@ class Evaluation_List_Metric:
             self.SUM = None if self.ALL is None else np.sum(self.ALL)
         self.STD = None if self.ALL is None else empty_list_std if len(self.ALL) == 0 else np.std(self.ALL)
 
-    def __getitem__(self, mode: ListMetricMode | str):
+    def __getitem__(self, mode: MetricMode | str):
         if self.error:
             raise MetricCouldNotBeComputedException(f"Metric {self.id} has not been calculated, add it to your eval_metrics")
-        if isinstance(mode, ListMetricMode):
+        if isinstance(mode, MetricMode):
             mode = mode.name
         if hasattr(self, mode):
             return getattr(self, mode)
@@ -98,9 +110,20 @@ class PanopticaResult(object):
         num_pred_instances: int,
         num_ref_instances: int,
         tp: int,
-        list_metrics: dict[ListMetric, list[float]],
+        list_metrics: dict[Metric, list[float]],
         edge_case_handler: EdgeCaseHandler,
     ):
+        """Result object for Panoptica, contains all calculatable metrics
+
+        Args:
+            reference_arr (np.ndarray): matched reference arr
+            prediction_arr (np.ndarray): matched prediction arr
+            num_pred_instances (int): number of prediction instances
+            num_ref_instances (int): number of reference instances
+            tp (int): number of true positives (matched instances)
+            list_metrics (dict[Metric, list[float]]): dictionary containing the metrics for each TP
+            edge_case_handler (EdgeCaseHandler): EdgeCaseHandler object that handles various forms of edge cases
+        """
         self._edge_case_handler = edge_case_handler
         empty_list_std = self._edge_case_handler.handle_empty_list_std()
         self._prediction_arr = prediction_arr
@@ -255,7 +278,7 @@ class PanopticaResult(object):
         ##################
         # List Metrics   #
         ##################
-        self._list_metrics: dict[ListMetric, Evaluation_List_Metric] = {}
+        self._list_metrics: dict[Metric, Evaluation_List_Metric] = {}
         for k, v in list_metrics.items():
             is_edge_case, edge_case_result = self._edge_case_handler.handle_zero_tp(
                 metric=k,
@@ -284,6 +307,11 @@ class PanopticaResult(object):
         return default_value
 
     def calculate_all(self, print_errors: bool = False):
+        """Calculates all possible metrics that can be derived
+
+        Args:
+            print_errors (bool, optional): If true, will print every metric that could not be computed and its reason. Defaults to False.
+        """
         metric_errors: dict[str, Exception] = {}
         for k, v in self._evaluation_metrics.items():
             try:
@@ -316,7 +344,7 @@ class PanopticaResult(object):
     def to_dict(self) -> dict:
         return self._evaluation_metrics
 
-    def get_list_metric(self, metric: ListMetric, mode: ListMetricMode):
+    def get_list_metric(self, metric: Metric, mode: MetricMode):
         if metric in self._list_metrics:
             return self._list_metrics[metric][mode]
         else:
@@ -387,11 +415,11 @@ def rq(res: PanopticaResult):
 
 #region IOU
 def sq(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.IOU, mode=ListMetricMode.AVG)
+    return res.get_list_metric(Metric.IOU, mode=MetricMode.AVG)
 
 
 def sq_std(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.IOU, mode=ListMetricMode.STD)
+    return res.get_list_metric(Metric.IOU, mode=MetricMode.STD)
 
 
 def pq(res: PanopticaResult):
@@ -400,11 +428,11 @@ def pq(res: PanopticaResult):
 
 #region DSC
 def sq_dsc(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.DSC, mode=ListMetricMode.AVG)
+    return res.get_list_metric(Metric.DSC, mode=MetricMode.AVG)
 
 
 def sq_dsc_std(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.DSC, mode=ListMetricMode.STD)
+    return res.get_list_metric(Metric.DSC, mode=MetricMode.STD)
 
 
 def pq_dsc(res: PanopticaResult):
@@ -413,11 +441,11 @@ def pq_dsc(res: PanopticaResult):
 
 #region clDSC
 def sq_cldsc(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.clDSC, mode=ListMetricMode.AVG)
+    return res.get_list_metric(Metric.clDSC, mode=MetricMode.AVG)
 
 
 def sq_cldsc_std(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.clDSC, mode=ListMetricMode.STD)
+    return res.get_list_metric(Metric.clDSC, mode=MetricMode.STD)
 
 
 def pq_cldsc(res: PanopticaResult):
@@ -426,14 +454,17 @@ def pq_cldsc(res: PanopticaResult):
 
 #region ASSD
 def sq_assd(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.ASSD, mode=ListMetricMode.AVG)
+    return res.get_list_metric(Metric.ASSD, mode=MetricMode.AVG)
 
 
 def sq_assd_std(res: PanopticaResult):
-    return res.get_list_metric(ListMetric.ASSD, mode=ListMetricMode.STD)
+    return res.get_list_metric(Metric.ASSD, mode=MetricMode.STD)
 #endregion
 
+#region Global
 def global_bin_dsc(res: PanopticaResult):
+    if res.tp == 0:
+        return 0.0
     pred_binary = res._prediction_arr.copy()
     ref_binary = res._reference_arr.copy()
     pred_binary[pred_binary != 0] = 1
@@ -441,19 +472,24 @@ def global_bin_dsc(res: PanopticaResult):
     return _compute_dice_coefficient(ref_binary, pred_binary)
 
 def global_bin_cldsc(res: PanopticaResult):
+    if res.tp == 0:
+        return 0.0
     pred_binary = res._prediction_arr.copy()
     ref_binary = res._reference_arr.copy()
     pred_binary[pred_binary != 0] = 1
     ref_binary[ref_binary != 0] = 1
     return _compute_centerline_dice_coefficient(ref_binary, pred_binary)
+#endregion
 
 
 if __name__ == "__main__":
     c = PanopticaResult(
+        reference_arr=np.zeros([5,5,5]),
+        prediction_arr=np.zeros([5,5,5]),
         num_ref_instances=2,
         num_pred_instances=5,
         tp=0,
-        list_metrics={ListMetric.IOU: []},
+        list_metrics={Metric.IOU: []},
         edge_case_handler=EdgeCaseHandler(),
     )
 