e1f7ba88c4a244e7c99f3e197cafc1652e6ecd2c
neuronflow <florian.kofler@tum.de>
2024-02-01 10:00:40 +0100

    sorted imports
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- unit_tests/test_datatype.py
+++ unit_tests/test_datatype.py
@@ -2,15 +2,16 @@
 # coverage run -m unittest
 # coverage report
 # coverage html
-import unittest
 import os
+import unittest
+
 import numpy as np
 
-from panoptica.panoptic_evaluator import Panoptic_Evaluator
 from panoptica.instance_approximator import ConnectedComponentsInstanceApproximator
-from panoptica.instance_matcher import NaiveThresholdMatching, MaximizeMergeMatching
-from panoptica.panoptic_result import PanopticaResult, MetricCouldNotBeComputedException
-from panoptica.metrics import _Metric, Metric, Metric, MetricMode
+from panoptica.instance_matcher import MaximizeMergeMatching, NaiveThresholdMatching
+from panoptica.metrics import Metric, MetricMode, _Metric
+from panoptica.panoptic_evaluator import Panoptic_Evaluator
+from panoptica.panoptic_result import MetricCouldNotBeComputedException, PanopticaResult
 from panoptica.utils.edge_case_handling import EdgeCaseHandler, EdgeCaseResult
 from panoptica.utils.processing_pair import SemanticPair
 
