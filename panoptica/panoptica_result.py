8bf894ec7b3e3e6c240b51b8e480b1c97e2c2191
Erik Großkopf <>
2026-05-20 16:18:22 +0200

    Review findingsOC

--- panoptica/panoptica_result.py
+++ panoptica/panoptica_result.py
@@ -406,7 +406,6 @@ class PanopticaResult(object):
             raise ValueError(
                 "unmatched ref voxel-count and volume lists must have equal length"
             )
-        self.is_matched: float = float("nan")
         # endregion
 
         # region Global
